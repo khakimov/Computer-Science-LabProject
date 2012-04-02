d3f46c56f46d441a426d9fef20f02475c20c1912
aleSuglia <alessandro.suglia@gmail.com>
2012-04-02 12:13:52 +0200

    new file:   OperationMatrixLinkedList/OpMatrix.c 	new file:   OperationMatrixLinkedList/OpMatrix.h 	new file:   OperationMatrixLinkedList/OperationMatrixLinkedList.cbp 	new file:   OperationMatrixLinkedList/OperationMatrixLinkedList.depend 	new file:   OperationMatrixLinkedList/OperationMatrixLinkedList.layout 	new file:   OperationMatrixLinkedList/ReadWriteFoo.c 	new file:   OperationMatrixLinkedList/ReadWriteFoo.h 	new file:   OperationMatrixLinkedList/main.c

---
+++ OperationMatrixLinkedList/ReadWriteFoo.h
@@ -0,0 +1,50 @@
+#ifndef _READWRITEFOO_H
+#define _READWRITEFOO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+#define MAXR 100
+#define MAXC 100
+#define MAXM 50
+
+#define val_access( i, n, j) ( i * n + j )
+
+#if defined _WIN32
+#define wait() getch()
+#elif defined __unix__
+#define wait() getchar()
+#endif
+
+struct Node
+{
+    float val;
+    struct Node *next;
+
+};
+
+typedef struct Node List;
+
+
+
+typedef struct
+{
+  List* mat;
+  int righe;
+  int colonne;
+  int id;
+
+
+}matrice;
+
+
+int leggiRighe( matrice * );
+int leggiColonne( matrice *);
+float scriviValore( int, int );
+float leggiValore( matrice *, int, int );
+void scriviRighe( matrice * );
+void scriviColonne( matrice * );
+void scriviElemento( matrice *, int, int, float );
+
+#endif
